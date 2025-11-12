a893cee923f9c8df4887ba103d1ba603fc0b33e3
Ankit0830 <ankitkumarsahu01299@gmail.com>
2025-11-12 18:01:58 +0530

    Add Payment Button also add Paytm Platform

---
+++ src/tripmanagementsystem1/Payment.java
@@ -0,0 +1,64 @@
+package tripmanagementsystem1;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Payment extends JFrame implements ActionListener
+{
+    JButton pay, back;
+    
+    Payment()
+    {
+        setBounds(500,200,800,600);
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/paytm.jpeg"));
+        Image i2 = i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0,0,800, 600);
+        add(image);
+        
+        
+        // Back Button
+        pay = new JButton("Pay");
+        pay.setBackground(Color.BLACK);
+        pay.setForeground(Color.WHITE);
+        pay.setBounds(420,0,80,40);
+        pay.addActionListener(this);
+        image.add(pay);
+        
+        
+        // Back Button
+        back = new JButton("Back");
+        back.setBackground(Color.BLACK);
+        back.setForeground(Color.WHITE);
+        back.setBounds(500,0,80,40);
+        back.addActionListener(this);
+        image.add(back);
+        
+        
+        
+        setVisible(true);
+    }
+    
+    @Override
+    public void actionPerformed (ActionEvent ae)
+    {
+        if (ae.getSource()  == pay)
+        {
+            
+        }
+        else 
+        {
+            setVisible(false);
+        }
+    }
+    
+    public static void main (String[] args)
+    {
+        new Payment();
+    }
+}
---
+++ src/tripmanagementsystem1/Paytm.java
@@ -0,0 +1,55 @@
+package tripmanagementsystem1;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Paytm extends JFrame implements ActionListener
+{
+    JButton back;
+    
+    Paytm()
+    {
+        setBounds(500,200,800,600);
+        
+        
+        JEditorPane pane = new JEditorPane();
+        pane.setEditable(false);
+        try 
+        {
+            pane.setPage("https://paytm.com/rent-payment");                 //Passed URL of paytm, we also use some another payment application like googlePay as well as PhonePay
+        }
+        catch (Exception e)
+        {
+            pane.setContentType("text/html");
+            pane.setText("<html> Could not load, Error 404</html>");
+        }
+        
+        JScrollPane sp = new JScrollPane(pane);
+        getContentPane().add(sp);
+        
+        // Back Button
+        back = new JButton("Back");
+        back.setBackground(Color.BLACK);
+        back.setForeground(Color.WHITE);
+        back.setBounds(690,20,80,30);
+        back.addActionListener(this);
+        pane.add(back);
+        
+        setVisible(true);
+        
+    }
+    
+    @Override
+    public void actionPerformed (ActionEvent ae)
+    {
+        setVisible(false);
+        new Payment();
+    }
+    
+    public static void main (String[] args)
+    {
+        new Paytm();
+    }
+}
